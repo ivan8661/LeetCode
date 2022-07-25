b0712c47eeddb7964c144ca1508025149c4d1830
ipltrk <ivan@poltorakov.ru>
2022-07-25 08:37:25 +0300

    hamming Weight ololololo, system function is slower than 15% AND memory weighter than 46% submissions in leetCode, HAHHAHAHAH

--- src/NumberOf1Bits/CodeClass.java
+++ src/NumberOf1Bits/CodeClass.java
@@ -7,11 +7,6 @@ public class CodeClass {
     }
 
     public static int hammingWeight(int n) {
-        int counter = 0;
-        while(n != 0){
-            counter += n&1;
-            n = n>>>1;
-        }
-        return counter;
+        return Integer.bitCount(n);
     }
 }
